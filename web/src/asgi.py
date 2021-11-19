5c52e49c4e2ce2972ea529fd749490c9d0d5142b
Nazarii <romanchenko@procivis.ch>
2021-11-19 22:52:16 +0200

    upd

--- web/src/asgi.py
+++ web/src/asgi.py
@@ -14,13 +14,13 @@ django.core.exceptions.AppRegistryNotReady: Apps aren't loaded yet.
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import OriginValidator
-from sockets.routing import websocket_urlpatterns as chart_urlpatterns
+# from sockets.routing import websocket_urlpatterns as chart_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     'websocket': AuthMiddlewareStack(
         URLRouter(
-            chart_urlpatterns,
+            [],
         )
     ),
 })
